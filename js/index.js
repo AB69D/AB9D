a3aea8cb31d112e155c7b6192b9df2b49d575f41
Md. Manzurul Islam <77693389+AB69D@users.noreply.github.com>
2024-05-19 02:09:11 +0600

    up

--- js/index.js
+++ js/index.js
@@ -1,69 +1,103 @@
-/* JS code used to start the animation */
 document.addEventListener('DOMContentLoaded', function() {
-    particleground(document.getElementById('animations'), {
-        dotColor: '#5cbdaa',
-        lineColor: '#5cbdaa'
-    });
-    var intro = document.getElementById('main_body');
-    intro.style.marginTop = -intro.offsetHeight / 2 + 'px';
-}, false);
+    // Start background animation
+    var canvas = document.getElementById('background-animation');
+    var ctx = canvas.getContext('2d');
 
-/* JS code used for text switcher */
-var textSwitcher = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = "";
-    this.tick();
-    this.isDeleting = false;
-};
+    // Set canvas size to window size
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-textSwitcher.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    // Generate random particles for background animation
+    var particles = [];
+    for (var i = 0; i < 100; i++) {
+        particles.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            radius: Math.random() * 3,
+            dx: Math.random() * 2 - 1,
+            dy: Math.random() * 2 - 1
+        });
     }
 
-    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) {
-        delta /= 2;
-    }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === "") {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-    }
-
-    setTimeout(function() {
-        that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    var elements = document.getElementsByClassName("text-switcher");
-    for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute("data-switch-content");
-        var period = elements[i].getAttribute("data-hold-time");
-        if (toRotate) {
-            new textSwitcher(elements[i], JSON.parse(toRotate), period);
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = '#5cbdaa';
+        for (var i = 0; i < particles.length; i++) {
+            var p = particles[i];
+            ctx.beginPath();
+            ctx.arc(p.x, p.y, p.radius, 0, Math.PI * 2, false);
+            ctx.fill();
+            p.x += p.dx;
+            p.y += p.dy;
+            if (p.x < 0 || p.x > canvas.width) {
+                p.dx = -p.dx;
+            }
+            if (p.y < 0 || p.y > canvas.height) {
+                p.dy = -p.dy;
+            }
         }
+        requestAnimationFrame(draw);
     }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".text-switcher > .wrap { border-right: 0.08em solid #666 }";
-    document.body.appendChild(css);
-};
+
+    draw();
+
+    // Text switcher
+    var textSwitcher = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = "";
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    textSwitcher.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) {
+            delta /= 2;
+        }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === "") {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(function() {
+            that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName("text-switcher");
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute("data-switch-content");
+            var period = elements[i].getAttribute("data-hold-time");
+            if (toRotate) {
+                new textSwitcher(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".text-switcher > .wrap { border-right: 0.08em solid #666 }";
+        document.body.appendChild(css);
+    };
+});
